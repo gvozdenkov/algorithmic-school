139517d6630afa5dd58a385366654475aac04f28
arty gvozdenkov <gvozdenkov@gmail.com>
2023-11-15 12:40:59 +0300

    fix: styles

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -17,25 +17,27 @@ export const MainPage = ({ extraClass = '' }: MainPageProps) => {
           им. Фибоначчи
         </p>
       </div>
-      <div className={clsx(styles.cards_box, styles.content__container)}>
-        <Link className={styles.link} to='/recursion'>
-          <div className={`${styles.card} ${styles.string}`} />
-        </Link>
-        <Link className={styles.link} to='/fibonacci'>
-          <div className={`${styles.card} ${styles.fibonacci}`} />
-        </Link>
-        <Link className={styles.link} to='/sorting'>
-          <div className={`${styles.card} ${styles.arr}`} />
-        </Link>
-        <Link className={styles.link} to='/stack'>
-          <div className={`${styles.card} ${styles.stack}`} />
-        </Link>
-        <Link className={styles.link} to='/queue'>
-          <div className={`${styles.card} ${styles.queue}`} />
-        </Link>
-        <Link className={styles.link} to='/list'>
-          <div className={`${styles.card} ${styles.list}`} />
-        </Link>
+      <div className={styles.contentWrapper}>
+        <div className={clsx(styles.cards_box)}>
+          <Link className={styles.link} to='/recursion'>
+            <div className={`${styles.card} ${styles.string}`} />
+          </Link>
+          <Link className={styles.link} to='/fibonacci'>
+            <div className={`${styles.card} ${styles.fibonacci}`} />
+          </Link>
+          <Link className={styles.link} to='/sorting'>
+            <div className={`${styles.card} ${styles.arr}`} />
+          </Link>
+          <Link className={styles.link} to='/stack'>
+            <div className={`${styles.card} ${styles.stack}`} />
+          </Link>
+          <Link className={styles.link} to='/queue'>
+            <div className={`${styles.card} ${styles.queue}`} />
+          </Link>
+          <Link className={styles.link} to='/list'>
+            <div className={`${styles.card} ${styles.list}`} />
+          </Link>
+        </div>
       </div>
       <Marquee className={styles.ticker} gradient={false} speed={200}>
         <p className={`text text_type_ticker text_color_secondary ${styles.ticker_text}`}>
