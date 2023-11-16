be71e7293046806177190ffcb19dde82642ce6a5
arty gvozdenkov <gvozdenkov@gmail.com>
2023-11-16 12:31:06 +0300

    refact: optimize fibonacci algo

--- src/pages/fibonacci-page/utils/fibonacci.ts
+++ src/pages/fibonacci-page/utils/fibonacci.ts
@@ -1,9 +1,18 @@
-const getFibonacciValue = (n: number): number => {
-  return n < 2 ? 1 : getFibonacciValue(n - 2) + getFibonacciValue(n - 1);
-};
+const getFibonacciValue = (n: number): number =>
+  n < 2 ? 1 : getFibonacciValue(n - 2) + getFibonacciValue(n - 1);
 
+const getFibonacciArray = (n: number): number[] =>
+  [...Array(n)].map((_, i) => getFibonacciValue(i));
+
+/**
+ * Generator. Return array of fib numbers form 0 to n-th index
+ *
+ * @yields {IterableIterator<number[]>} array with +1 fibonacci number
+ */
 export function* fibonacci(n: number): IterableIterator<number[]> {
-  for (let i = 0; i <= n; i++) {
-    yield [...Array(i + 1)].map((_, index) => getFibonacciValue(index));
+  const fibArray = getFibonacciArray(n + 1);
+
+  for (let i = 0; i <= n + 1; i++) {
+    yield fibArray.slice(0, i);
   }
 }
